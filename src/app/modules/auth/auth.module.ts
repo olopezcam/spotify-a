bf757ccd965c99ad2c6985a4e78def748a47b60c
olopezcam <olopezcam@gmail.com>
2024-04-18 15:30:46 +0200

    login form

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthPageComponent } from './pages/auth-page/auth-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 
 @NgModule({
   declarations: [
-    AuthPageComponent
+    AuthPageComponent,
+    LoginPageComponent
   ],
   imports: [
     CommonModule,
